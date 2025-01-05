f8aac91c1ff78f4957a884f8f201c8520fe9823b
Kosta Gacina <psnkoleg@gmail.com>
2025-01-05 16:02:31 +0100

    added some basic styling

--- seeds/index.js
+++ seeds/index.js
@@ -16,9 +16,13 @@ const seedDB = async () => {
   await Campground.deleteMany({});
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
       title: `${sample(descriptors)} ${sample(places)}`,
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
+      image: `https://picsum.photos/400?random=${Math.random()}`,
+      description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ea repellendus minima dignissimos esse cupiditate iure adipisci repellat? Adipisci expedita autem est iusto. Deleniti voluptates quasi explicabo tenetur consequuntur, a laborum!',
+      price
     });
     await camp.save();
   }
